7f9ee5b6396c91544425dc7616b00505c01df3ff
lirongqi <lirongqi@zhenghe.cn>
2025-04-05 16:46:17 +0800

    init

---
+++ src/main/java/com/rq/cloudpicturebackend/CloudPictureBackendApplication.java
@@ -0,0 +1,17 @@
+package com.rq.cloudpicturebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.rq.cloudpicturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class CloudPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rq/cloudpicturebackend/CloudPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rq.cloudpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
